78ed25d72b2ffb5eb321676e81e0dcf61f54a1aa
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-07-08 23:58:52 +0530

    Time: 4 ms (3.67%), Space: 12.4 MB (53.15%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        
+        return (word == word.upper() or
+                word == word.lower() or
+                (word[0].isupper() and word[1:] == word[1:].lower()))
