1f06cdde90e46473763d779a78e6e9bc2d38b61d
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-07-13 21:03:30 +0530

    Time: 703 ms (9.23%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,4 +1,4 @@
-select p.project_id,ROUND(AVG(e.experience_years), 2)as average_years
+select p.project_id,ROUND(AVG(e.experience_years), 2)as average_years 
 from Project p
 left join Employee e
 on p.employee_id = e.employee_id
