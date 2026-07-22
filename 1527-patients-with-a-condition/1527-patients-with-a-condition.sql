1ae0426e50f51f95bbc2a766cdb2a3d23a902b18
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-07-22 21:01:19 +0530

    Time: 980 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,9 @@
+select *
+from Patients
+WHERE conditions like 'DIAB1%' OR conditions like '%DIAB1' OR conditions like '% DIAB1%'
+      
+#'ABC%' → ABC se start
+#'%ABC' → ABC pe end
+#'% ABC%' → Space ke baad ABC (naya word)
+#'%ABC%' → ABC kahin bhi
+
