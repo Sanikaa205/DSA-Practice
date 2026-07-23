9c07216d750f09bf18f983675ea9c7fc202114f6
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-07-23 21:33:58 +0530

    Time: 1080 ms (5.9%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,7 +3,7 @@ select p.product_name,
 from Products p
 join Orders o
 on p.product_id = o.product_id
-WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+WHERE order_date LIKE '2020-02%'
 group by p.product_id,p.product_name
 having sum(o.unit) >= 100
 
