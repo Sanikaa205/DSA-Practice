75caa6513504772b2af17a357d4b487b8f86f1a3
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-06 17:24:06 +0530

    Time: 30 ms (91.68%), Space: 48.6 MB (50.8%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n : candyType)
+        {
+            set.add(n);
+        }
+
+        return Math.min(set.size(),candyType.length/2);
+        
+    }
+}
