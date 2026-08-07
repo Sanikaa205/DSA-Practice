2880b64bac3892da48e1b9401fd98417ff819744
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-07 19:39:59 +0530

    Time: 38 ms (7.87%), Space: 46.1 MB (99.66%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -5,7 +5,13 @@ class Solution {
         for(int i = 0 ; i< s.length();i++)
         {
             char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            if(map.containsKey(ch))
+            {
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch ,1);
+            }
 
         }
 
