e3cb4922e74a9d19b085fa22fd2b4358cd37a538
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-08 15:33:44 +0530

    Time: 1 ms (83.56%), Space: 43 MB (18.35%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+
+        HashMap<Character,String> map1 = new HashMap<>();
+        HashMap<String ,Character > map2 = new HashMap<>();
+
+        String[] words = s.split(" ");
+
+        if(pattern.length() != words.length)
+        {
+            return false;
+        }
+
+        for(int i = 0;i < pattern.length();i++)
+        {
+            char ch1 = pattern.charAt(i);
+            String ch2 = words[i];
+            
+            if(map1.containsKey(ch1))
+            {
+                if(!map1.get(ch1).equals(ch2)) return false;
+            }
+
+            if(map2.containsKey(ch2))
+            {
+                if(map2.get(ch2) != ch1) return false;
+            }
+
+            map1.put(ch1,ch2);
+            map2.put(ch2,ch1);
+        }
+        
+        return true;
+        
+    }
+}
