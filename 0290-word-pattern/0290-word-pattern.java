5a4a2ffeb604e15b5b71e23621580aa33d7a6b0c
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-08 15:59:05 +0530

    Time: 1 ms (83.56%), Space: 42.6 MB (74.41%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
 
-        HashMap<Character,String> map1 = new HashMap<>();
-        HashMap<String ,Character > map2 = new HashMap<>();
+        HashMap<Character , String> map1 = new HashMap<>();
+        HashMap<String , Character> map2 = new HashMap<>();
 
-        String[] words = s.split(" ");
+        String []words = s.split(" ");
 
-        if(pattern.length() != words.length)
-        {
-            return false;
-        }
+        if(pattern.length() != words.length) return false;
 
-        for(int i = 0;i < pattern.length();i++)
+        for(int i = 0; i< words.length;i++)
         {
             char ch1 = pattern.charAt(i);
             String ch2 = words[i];
-            
+
             if(map1.containsKey(ch1))
             {
                 if(!map1.get(ch1).equals(ch2)) return false;
@@ -23,14 +20,13 @@ class Solution {
 
             if(map2.containsKey(ch2))
             {
-                if(map2.get(ch2) != ch1) return false;
+                 if (map2.get(ch2)!=ch1) return false;
             }
 
             map1.put(ch1,ch2);
             map2.put(ch2,ch1);
+
         }
-        
         return true;
-        
     }
 }
