48bfa9c70ec5e56508a9e9d8f91e122838304a35
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-08 14:40:24 +0530

    Time: 25 ms (8.34%), Space: 44.2 MB (16.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -11,19 +11,12 @@ class Solution {
 
             if(map1.containsKey(ch1))
             {
-                if(map1.get(ch1) != ch2)
-                {
-                    return false;
-                }
-
+                if(map1.get(ch1) != ch2) return false;
             }
 
             if(map2.containsKey(ch2))
             {
-                if(map2.get(ch2) != ch1)
-                {
-                    return false;
-                }
+                if(map2.get(ch2) != ch1) return false;
             }
 
             map1.put(ch1,ch2);
