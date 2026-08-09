ead7167e86e1cac8a33e919594936891762aa8be
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-09 11:32:09 +0530

    Time: 0 ms (100%), Space: 43.3 MB (80.2%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,8 +1,7 @@
-class Solution 
-{
-    public int[] plusOne(int[] digits)
-     {
-       for (int i = digits.length-1;i>=0;i--)
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for(int i = digits.length -1 ; i >= 0 ;i--)
         {
             while(digits[i] < 9)
             {
@@ -10,13 +9,13 @@ class Solution
                 return digits;
             }
 
-            digits[i] = 0;
+            digits[i]=0;
         }
 
-       int[] result = new int[digits.length+1];  // necessary for carry
-       result[0]=1;
-       return result; 
+        int []result = new int[digits.length+1];
+        result[0] =1;
 
+        return result;
         
     }
 }
