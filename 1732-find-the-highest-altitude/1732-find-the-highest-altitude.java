bcba2032afbb10682625ce4c0442e91f1240c798
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-08-17 17:24:48 +0530

    Time: 0 ms (100%), Space: 42.9 MB (88.73%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+
+        int high = 0;
+        int current = 0;
+        for(int i = 0 ; i <gain.length;i++)
+        {
+            current = current + gain[i];
+            high = Math.max(current , high);
+        }
+
+        return high;
+        
+    }
+}
